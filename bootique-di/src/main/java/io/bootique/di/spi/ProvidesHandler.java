86a9d66cf881dd654279afc6248a8032005b4a64
Andrus Adamchik <andrus@objectstyle.com>
2018-04-02 20:27:53 +0300

    Support for provider methods in modules #1
    * runtime annotation check-based impl - in progress

--- bootique-di/src/main/java/io/bootique/di/spi/ProvidesHandler.java
+++ bootique-di/src/main/java/io/bootique/di/spi/ProvidesHandler.java
@@ -1,15 +1,13 @@
 package io.bootique.di.spi;
 
-import io.bootique.di.Key;
 import io.bootique.di.Module;
 
-import javax.inject.Provider;
 import java.util.Collection;
-import java.util.Map;
+import java.util.Collections;
 
 class ProvidesHandler {
 
     static Collection<KeyBindingPair<?>> bindingsFromProviderMethods(Module module) {
-
+        return Collections.emptyList();
     }
 }
